9adcd94b257448028a10017756f4cf82039b8880
FraFra2 <kekkino41@gmail.com>
2023-07-05 21:49:18 +0200

    ADD 10 Email Array

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            msg: "snaso"
-        };
-    },
-    methods: {
-        returnEmail(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(resp){
-                const mail = resp.data;
-                console.log("QUESTA MAIL", mail.response);
-            });
-        }
-        
+  data() {
+    return {
+      mails: []
+    };
+  },
+  methods: {
+    returnEmail() {
+      for (let index = 0; index < 10; index++) {
+        axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((resp) => {
+            const mail = resp.data.response;
+            this.mails.push(mail);
+          })
+      }
     }
+  },
+  mounted() {
+    this.returnEmail();
+  }
 }).mount('#app');
